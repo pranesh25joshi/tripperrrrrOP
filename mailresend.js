6908f06610faa2db9a367076e43d53963a4b3319
Pranesh Joshi <139208152+pranesh25joshi@users.noreply.github.com>
2026-06-11 21:50:19 +0530

    updating resend conf

--- mailresend.js
+++ mailresend.js
@@ -3,7 +3,7 @@ import { Resend } from 'resend';
 const resend = new Resend('re_BFsXezEz_PCqS2Qh24e3xnk6SQuVwvbCU');
 
 await resend.emails.send({
-  from: 'Pranesh <invitetrips@pranesh.xyz>',
+  from: 'Pranesh <invitetrips@iampranesh.me>',
   to: ['nimeshjoshi619@gmail.com'],
   subject: 'hello world',
   html: '<p>it works!</p>',
